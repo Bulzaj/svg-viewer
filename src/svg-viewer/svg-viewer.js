b5d7d018fab9ddbc05a00301658d1a648d49a9f7
bulzaj <ernestsado94@gmail.com>
2021-11-28 19:57:45 +0100

    add zooming mouse event

--- src/svg-viewer/svg-viewer.js
+++ src/svg-viewer/svg-viewer.js
@@ -20,6 +20,16 @@ const transformMatrixReducer = (state, action) => {
         ...state,
         transformMatrix: pan(action.dx, action.dy, state.transformMatrix)
       }
+    case 'ZOOM':
+      return {
+        ...state,
+        transformMatrix: zoom(
+          action.scaleDelta,
+          action.x,
+          action.y,
+          state.transformMatrix
+        )
+      }
     case 'ZOOM_TO_CENTER':
       return {
         ...state,
@@ -124,6 +134,7 @@ const SvgViewer = (props) => {
   }
 
   const mouseMoveHandler = (e) => {
+    e.preventDefault()
     if (!isPanning) return
 
     const dx = !props.invertXPanning ? e.movementX : -e.movementX
@@ -138,6 +149,23 @@ const SvgViewer = (props) => {
 
   const mouseUpHandler = () => {
     setIsPanning(false)
+  }
+
+  const mouseWheelHandler = (e) => {
+    const x = e.clientX
+    const y = e.clientY
+
+    const scaleDelta =
+      e.deltaY > 0
+        ? props.navigationPane.zoomFactor
+        : 1 / props.navigationPane.zoomFactor
+
+    dispatch({
+      type: 'ZOOM',
+      scaleDelta,
+      x,
+      y
+    })
   }
 
   let displayData = (
@@ -177,6 +205,7 @@ const SvgViewer = (props) => {
         onMouseDown={mouseDownHandler}
         onMouseMove={mouseMoveHandler}
         onMouseUp={mouseUpHandler}
+        onWheel={mouseWheelHandler}
       >
         <g id='root-group' transform={`matrix(${state.transformMatrix})`}>
           {displayData}
